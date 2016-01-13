a393adddbc5abf3c0c609356d2555c4186e52113
root <root@acn-log01.prod.itc.homecredit.cn>
2016-01-14 05:01:30 +0800

    cleanup

--- bin/check_redis_keys.sh
+++ bin/check_redis_keys.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 vals=""
-for i in apache weblogic hsapp; 
+for i in apache apache_rotated weblogic hsapp; 
 do 
 	var=$(redis-cli debug OBJECT $i|awk '{print $5}'|cut -b 18-)
 	[ -z $var ] && var=0
