9f3f1afa45696f5327141deb16501893b4247d4a
michaleka <jan.michalek@embedit.cz>
2016-01-29 12:10:57 +0100

    remote update

---
+++ bin/update_from_git.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+cd /opt/elk
+git fetch
+if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ];then
+    echo "Need to pull"
+    git pull
+    for i in /etc/init.d/logstash* ;do
+    	$i restart
+    done 
+fi
