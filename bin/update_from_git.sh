5297168806f28828d851ec4dc8dcb4cacad7a9af
Jan.Michalek <Jan.Michalek@N0805.hcg.homecredit.net>
2016-01-29 08:06:57 +0100

    .

--- bin/update_from_git.sh
+++ bin/update_from_git.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 cd /opt/elk
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse @{u})
-BASE=$(git merge-base @ @{u})
-
-if [ $LOCAL = $REMOTE ]; then
-    echo "Up-to-date"
-elif [ $LOCAL = $BASE ]; then
+git fetch
+if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ];then
     echo "Need to pull"
     git pull
     for i in /etc/init.d/logstash* ;do
     	$i restart
     done 
-elif [ $REMOTE = $BASE ]; then
-    echo "Need to push"
-else
-    echo "Diverged"
 fi
