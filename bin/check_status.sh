b4e5e6ebcd52bd7ab718a4973982cce2b6428766
jan.michalek <jan.michalek@N0908.hcg.homecredit.net>
2016-05-04 11:19:48 +0200

    added copy of .err file for investigation

--- bin/check_status.sh
+++ bin/check_status.sh
@@ -4,6 +4,9 @@ for i in /etc/init.d/logstash* ;do
 	if [ -e $i ] ; then
 		$i status &>/dev/null
 		if [ $? -ne 0 ]; then
+			if [[ $(basename $i) == logstash* ]];then
+				cp /log/logstash/$(basename $i).err /log/logstash/$(basename $i).err_$(date +%F_%H-%M)
+			fi 
 			echo "$i is not running, now restarting" |tee >(mail -s "$HOSTNAME" jan.michalek@embedit.cz)
 			$i restart
 			exit 1
