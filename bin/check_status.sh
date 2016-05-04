99eb1009b29bb4ebad0f4f3727d4882a8b55c0b5
jan.michalek <jan.michalek@N0908.hcg.homecredit.net>
2016-05-04 11:21:50 +0200

    added info about file to mail

--- bin/check_status.sh
+++ bin/check_status.sh
@@ -4,10 +4,12 @@ for i in /etc/init.d/logstash* ;do
 	if [ -e $i ] ; then
 		$i status &>/dev/null
 		if [ $? -ne 0 ]; then
+			FNAME=""
 			if [[ $(basename $i) == logstash* ]];then
+				FNAME="/log/logstash/$(basename $i).err_$(date +%F_%H-%M)"
 				cp /log/logstash/$(basename $i).err /log/logstash/$(basename $i).err_$(date +%F_%H-%M)
 			fi 
-			echo "$i is not running, now restarting" |tee >(mail -s "$HOSTNAME" jan.michalek@embedit.cz)
+			echo "$i is not running, now restarting. $FNAME" |tee >(mail -s "$HOSTNAME" jan.michalek@embedit.cz)
 			$i restart
 			exit 1
 		fi
