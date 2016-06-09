1180f0265bd59c16219024ae4a96689d8a7157b3
Jan.Michalek <Jan.Michalek@N0908.hcg.homecredit.net>
2016-06-09 11:51:45 +0200

    changed mail dest for warnings

--- bin/check_status.sh
+++ bin/check_status.sh
@@ -9,7 +9,7 @@ for i in /etc/init.d/logstash* ;do
 				FNAME="/log/logstash/$(basename $i).err_$(date +%F_%H-%M)"
 				cp /log/logstash/$(basename $i).err /log/logstash/$(basename $i).err_$(date +%F_%H-%M)
 			fi 
-			echo "$i is not running, now restarting. $FNAME" |tee >(mail -s "$HOSTNAME" jan.michalek@embedit.cz)
+			echo "$i is not running, restarting. $FNAME" |tee >(mail -s "$HOSTNAME" middleware_run@homecredit.net)
 			$i restart
 			exit 1
 		fi
