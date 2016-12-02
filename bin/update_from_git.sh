90ef83d3750582f7df494b7ba5b4f37314cfb95e
Jan.Michalek <Jan.Michalek@N0908.hcg.homecredit.net>
2016-12-02 13:08:05 +0100

    .

--- bin/update_from_git.sh
+++ bin/update_from_git.sh
@@ -4,7 +4,5 @@ git fetch
 if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ];then
     echo "Need to pull"
     git pull
-    for i in /etc/init.d/logstash* ;do
-    	$i restart
-    done 
+    systemctl restart logstash 
 fi
