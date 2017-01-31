701fde2e8766b5920c50d987ccdf8df767bce5a7
Jan.Michalek <Jan.Michalek@N0908.hcg.homecredit.net>
2017-01-31 13:00:47 +0100

    .

--- bin/elasticsearch_clean_old.sh
+++ bin/elasticsearch_clean_old.sh
@@ -5,10 +5,4 @@ if [ $# -ne 1 ] ;then
 else
 	def=$1
 fi
-if [ -e /usr/bin/curator_cli ] ;then 
-	# elastic 5.x
-	curator_cli --host localhost delete_indices --older-than $def --time-unit days --timestring '%Y.%m.%d'
-else 
-	# elastic 2.x
-	curator --host localhost delete indices --older-than $def --time-unit days --timestring '%Y.%m.%d'
-fi
+curator --host localhost delete indices --older-than $def --time-unit days --timestring '%Y.%m.%d'
