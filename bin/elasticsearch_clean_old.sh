b6adbaabb90dea60d67cb5e4a7e7a2efd232af82
Jan.Michalek <Jan.Michalek@N0908.hcg.homecredit.net>
2017-01-31 12:58:50 +0100

    diff curator 2x vs 5x

--- bin/elasticsearch_clean_old.sh
+++ bin/elasticsearch_clean_old.sh
@@ -5,4 +5,10 @@ if [ $# -ne 1 ] ;then
 else
 	def=$1
 fi
-curator_cli --host localhost delete_indices --older-than $def --time-unit days --timestring '%Y.%m.%d'
+if [ -e /usr/bin/curator_cli ] ;then 
+	# elastic 5.x
+	curator_cli --host localhost delete_indices --older-than $def --time-unit days --timestring '%Y.%m.%d'
+else 
+	# elastic 2.x
+	curator --host localhost delete indices --older-than $def --time-unit days --timestring '%Y.%m.%d'
+fi
