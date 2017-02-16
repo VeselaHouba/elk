8666e8c2ac4b238fee62939fe0850cb296812447
Jan.Michalek <Jan.Michalek@N0908.hcg.homecredit.net>
2017-02-16 11:27:46 +0100

    fixed update script

--- bin/update_from_git.sh
+++ bin/update_from_git.sh
@@ -1,10 +1,9 @@
-#!/bin/bash -x
+#!/bin/bash
 cd /opt/elk
 git fetch
 if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ];then
 	echo "Need to pull"
 	git pull
-	#systemctl restart logstash
-	service logstash restart
+	service logstash restart || systemctl restart logstash
 	[[ -e /etc/init.d/logstash_2 ]] && service logstash_2 restart  
 fi
