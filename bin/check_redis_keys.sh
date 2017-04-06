27538008be38de1fa6c8db3522ed6ca4ff174b7c
Jan.Michalek <Jan.Michalek@N0908.hcg.homecredit.net>
2017-04-06 18:16:53 +0200

    merged with latest us

--- bin/check_redis_keys.sh
+++ bin/check_redis_keys.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
 PATH=$PATH:/opt/redis/bin/
 uname -n
@@ -21,27 +20,3 @@ done
 header="=Key= =Lenght= =Difference=\n"
 echo -e $header$vals|column -t
 echo "###############################################################"
-=======
-#!/bin/bash
-PATH=$PATH:/opt/redis/bin/
-uname -n
-uptime | grep -ohe 'load average[s:][: ].*' | awk '{ print $3" "$4" "$5 }'
-echo ""
-vals=""
-keys=$(redis-cli keys \* 2>/dev/null|sort)
-for i in $keys; 
-do 
-	var=$(redis-cli debug OBJECT $i 2>/dev/null|awk '{print $5}'|cut -b 18-)
-	[ -z $var ] && var=0
-	# load value
-	pvar=`cat /tmp/$i 2>/dev/null`
-	[ -z $pvar ] && pvar=0
-	# save value
-	echo $var > /tmp/$i
-
-	vals=${vals}${i}" "${var}" "$(expr $var - $pvar)"\n"
-done
-header="=Key= =Lenght= =Difference=\n"
-echo -e $header$vals|column -t
-echo "###############################################################"
->>>>>>> refs/heads/us_prod
