e7fdaf3e79ba34b83a2a9a5d755ff7a7a71d92fb
root <root@scnvx040.prod.itc.homecredit.cn>
2017-05-18 21:07:36 +0800

    .

---
+++ bin/restart_elastic.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Restarts servers based on current hour modulo 7 - rotate one by one 
+
+HOUR=$(date +%H)
+HOUR=$(( HOUR%7 ))
+target=scnvx04${HOUR}.prod.itc.homecredit.cn
+#echo $target
+local=$(uname -n)
+if [ $local = $target ] ;then
+	service elasticsearch restart &>/dev/null
+fi
