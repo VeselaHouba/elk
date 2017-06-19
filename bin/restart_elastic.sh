1a2c683177745294e6ef5d922c216791e7d065f6
jan.michalek <jan.michalek@N0908.hcg.homecredit.net>
2017-06-19 09:09:01 +0200

    minor fix

--- bin/restart_elastic.sh
+++ bin/restart_elastic.sh
@@ -2,6 +2,8 @@
 # Restarts servers based on current hour modulo 7 - rotate one by one 
 
 HOUR=$(date +%H)
+# bash interprets 08 as octal -> force to int
+HOUR=${HOUR#0}
 HOUR=$(( HOUR%7 ))
 target=scnvx04${HOUR}.prod.itc.homecredit.cn
 #echo $target
