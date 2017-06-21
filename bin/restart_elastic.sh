b15bf32d86f1abdf9038bebdd4e1237ec604407a
jan.michalek <jan.michalek@N0908.hcg.homecredit.net>
2017-06-21 09:54:57 +0200

    elastic restart for US

--- bin/restart_elastic.sh
+++ bin/restart_elastic.sh
@@ -5,10 +5,20 @@ HOUR=$(date +%H)
 # bash interprets 08 as octal -> force to int
 HOUR=${HOUR#0}
 HOUR=$(( HOUR%7 ))
+
+########## CN
 target=scnvx04${HOUR}.prod.itc.homecredit.cn
 #echo $target
 local=$(uname -n)
 if [ $local = $target ] ;then
 	systemctl restart elasticsearch &>/dev/null
 	#echo "restarted $target"|mail -s "CN restart" jan.michalek@embedit.cz
+fi
+########### US
+target=pxy0${HOUR}-uslogs1.us.prod
+#echo $target
+local=$(uname -n)
+if [ $local = $target ] ;then
+	systemctl restart elasticsearch &>/dev/null
+	#echo "restarted $target"|mail -s "US restart" jan.michalek@embedit.cz
 fi
